b3a0fc402631f092274d344eb79f11abcb40bdf4
Rifa'i02 <fumu8755@gmail.com>
2022-11-23 10:35:33 +0700

    kategori

---
+++ resources/views/admin/kategori.blade.php
@@ -0,0 +1,165 @@
+@extends('admin.layout.mainlayout')
+@section('title', 'Kategori')
+
+
+@section('content')
+
+    <div class="pagetitle">
+        <h1>Kategori Produk</h1>
+        <nav>
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><span>Home</span></li>
+            <li class="breadcrumb-item active">Kategori</li>
+          </ol>
+        </nav>
+      </div><!-- End Page Title -->
+    
+      <!-- Form Input -->
+      <section class="section dashboard">
+        <div class="row justify-content-center">
+    
+          <!-- Left side columns -->
+          <div class="col-lg-12">
+            <div class="row">
+    
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">General Form Elements</h5>
+        
+                      <!-- General Form Elements -->
+                      <form>
+
+                        <div class="row mb-4">
+                          <label for="inputText" class="col-sm-2 col-form-label">Nama:</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control">
+                          </div>
+                        </div>
+
+                        <div class="row mb-4">
+                          <label for="inputNumber" class="col-sm-2 col-form-label">Gambar:</label>
+                          <div class="col-sm-10">
+                            <input class="form-control" type="file" id="formFile">
+                          </div>
+                        </div>
+        
+                        <div class="row mb-4 text-end">
+                          <div class="col-sm-12">
+                            <button type="submit" class="btn btn-primary">Tambah Kategori</button>
+                          </div>
+                        </div>
+        
+                      </form><!-- End General Form Elements -->
+        
+                    </div>
+                  </div>
+    
+            </div>
+          </div><!-- End Left side columns -->
+    
+        </div>
+       </section><!-- End Form Input -->
+
+       <!-- Tables -->
+       <section class="section">
+        <div class="row">
+          <div class="col-lg-12">
+  
+            <div class="card table-responsive">
+              <div class="card-body">
+                <h5 class="card-title">Datatables</h5>
+                <p>Add lightweight datatables to your project with using the <a href="https://github.com/fiduswriter/Simple-DataTables" target="_blank">Simple DataTables</a> library. Just add <code>.datatable</code> class name to any table you wish to conver to a datatable</p>
+  
+                <!-- Table with stripped rows -->
+                <table class="table datatable">
+                  <thead>
+                    <tr>
+                      <th scope="col">No</th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Position</th>
+                      <th scope="col">Age</th>
+                      <th scope="col">Start Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Brandon Jacob</td>
+                      <td>Designer</td>
+                      <td>28</td>
+                      <td>2016-05-25</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Bridie Kessler</td>
+                      <td>Developer</td>
+                      <td>35</td>
+                      <td>2014-12-05</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td>Ashleigh Langosh</td>
+                      <td>Finance</td>
+                      <td>45</td>
+                      <td>2011-08-12</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">4</th>
+                      <td>Angus Grady</td>
+                      <td>HR</td>
+                      <td>34</td>
+                      <td>2012-06-11</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">5</th>
+                      <td>Raheem Lehner</td>
+                      <td>Dynamic Division Officer</td>
+                      <td>47</td>
+                      <td>2011-04-19</td>
+                      <tr>
+                      <th scope="row">1</th>
+                      <td>Brandon Jacob</td>
+                      <td>Designer</td>
+                      <td>28</td>
+                      <td>2016-05-25</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Bridie Kessler</td>
+                      <td>Developer</td>
+                      <td>35</td>
+                      <td>2014-12-05</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td>Ashleigh Langosh</td>
+                      <td>Finance</td>
+                      <td>45</td>
+                      <td>2011-08-12</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">4</th>
+                      <td>Angus Grady</td>
+                      <td>HR</td>
+                      <td>34</td>
+                      <td>2012-06-11</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">5</th>
+                      <td>Raheem Lehner</td>
+                      <td>Dynamic Division Officer</td>
+                      <td>47</td>
+                      <td>2011-04-19</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <!-- End Table with stripped rows -->
+  
+              </div>
+            </div>
+  
+          </div>
+        </div>
+      </section><!-- End Tables -->
+
+  @endsection
